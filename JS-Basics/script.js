798056d7ab90aa96dbbb7d946067cf97a4760c0a
Guillermo Olmedo <geomormx@gmail.com>
2020-02-27 00:30:56 +0800

    JS Basics: Variable mutation, type coercion

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -1,6 +1,8 @@
 /**
  * Variables and data types
  */
+/*
+
 var firstName = "John";
 console.log(firstName);
 
@@ -15,3 +17,45 @@ console.log(job);
 
 job = "Teacher";
 console.log(job);
+*/
+
+/**
+ * Variable mutation and type coercion
+ */
+
+var firstName = "John";
+var age = 28;
+
+// Type coercion
+console.log(firstName + " " + age);
+
+var job, isMarried;
+job = "teacher";
+isMarried = false;
+
+console.log(
+  firstName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is he married? " +
+    isMarried
+);
+
+// Variable mutation
+age = "twenty eight";
+job = "driver";
+
+alert(
+  firstName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is he married? " +
+    isMarried
+);
+
+var lastName = prompt("What is his last name?");
+console.log(firstName + " " + lastName);
