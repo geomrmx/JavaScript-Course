9d7e11eb7e5fe4b25d73a070d001543e1cca57e3
Guillermo Olmedo <geomormx@gmail.com>
2020-02-27 02:57:13 +0800

    JS Basics: Boolean logic

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -128,7 +128,7 @@ console.log(x);
 /**
  * If / else statements
  */
-
+/*
 var firstName = "John";
 var civilStatus = "single";
 
@@ -143,4 +143,22 @@ if (isMarried) {
   console.log(firstName + " is married!");
 } else {
   console.log(firstName + " will hopefully marry soon :)");
+}
+*/
+
+/**
+ * Boolean logic
+ */
+
+var firstName = "John";
+var age = 20;
+
+if (age < 13) {
+  console.log(firstName + " is a boy.");
+} else if (age >= 13 && age < 20) {
+  console.log(firstName + " is a teenager.");
+} else if (age >= 20 && age < 30) {
+  console.log(firstName + " is a young man.");
+} else {
+  console.log(firstName + " is a man.");
 }
