6bde7af9cdf4bfa12ca2fdf291acb6f922502e58
Guillermo Olmedo <geomormx@gmail.com>
2020-02-27 02:41:07 +0800

    JS Basics: `if ~ else` statement

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -124,3 +124,23 @@ console.log(x);
 x--;
 console.log(x);
 */
+
+/**
+ * If / else statements
+ */
+
+var firstName = "John";
+var civilStatus = "single";
+
+if (civilStatus === "married") {
+  console.log(firstName + " is married!");
+} else {
+  console.log(firstName + " will hopefully marry soon :)");
+}
+
+var isMarried = true;
+if (isMarried) {
+  console.log(firstName + " is married!");
+} else {
+  console.log(firstName + " will hopefully marry soon :)");
+}
