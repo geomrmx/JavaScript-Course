28668d94bddbeefe771c44efd94d84bf4693aec2
Guillermo Olmedo <geomormx@gmail.com>
2020-02-26 23:04:35 +0800

    JS Basics: Variables and data types

---
+++ JS-Basics/script.js
@@ -0,0 +1,17 @@
+/**
+ * Variables and data types
+ */
+var firstName = "John";
+console.log(firstName);
+
+var lastName = "Smith";
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = "Teacher";
+console.log(job);
