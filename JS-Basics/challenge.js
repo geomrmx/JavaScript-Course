c86ebcc0e8d2b2f6487e1d167177e6f10132471d
Guillermo Olmedo <geomormx@gmail.com>
2020-02-27 23:47:10 +0800

    JS Basics: Coding challenge 2

--- JS-Basics/challenge.js
+++ JS-Basics/challenge.js
@@ -10,7 +10,7 @@ Mark and John are trying to compare their BMI (Body Mass Index), which is calcul
 3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
 4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true").
  */
-
+/*
 var massMark = 78; // kg
 var heightMark = 1.69; // meters
 
@@ -24,3 +24,43 @@ console.log(markBMI, johnBMI);
 
 var isMarkHigh = markBMI > johnBMI;
 console.log("Is Mark's BMI higher than John's? " + isMarkHigh);
+*/
+/**
+ * CODING CHALLENGE 2
+ */
+
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team.
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score).
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision.
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+ */
+
+var scoreJohn = (89 + 120 + 103) / 3;
+var scoreMike = (116 + 94 + 123) / 3;
+console.log(scoreJohn, scoreMike);
+
+if (scoreJohn > scoreMike) {
+  console.log("John's team wins with " + scoreJohn + " points");
+} else if (scoreMike > scoreJohn) {
+  console.log("Mike's team wins with " + scoreMike + " points");
+} else {
+  console.log("There is a draw");
+}
+
+var scoreMary = (97 + 134 + 105) / 3;
+console.log(scoreJohn, scoreMike, scoreMary);
+
+if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
+  console.log("John's team wins with " + scoreJohn + " points");
+} else if (scoreMike > scoreJohn && scoreMike > scoreMary) {
+  console.log("Mike's team wins with " + scoreMike + " points");
+} else if (scoreMary > scoreJohn && scoreMary > scoreMike) {
+  console.log("Mary's team wins with " + scoreMary + " points");
+} else {
+  console.log("There is a draw");
+}
