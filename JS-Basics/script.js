502a1d5f1548eb491898b0d2691fb51b378202c5
Guillermo Olmedo <geomormx@gmail.com>
2020-02-27 22:02:00 +0800

    JS Basics: Ternary operator, `switch` statement

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -149,7 +149,7 @@ if (isMarried) {
 /**
  * Boolean logic
  */
-
+/*
 var firstName = "John";
 var age = 20;
 
@@ -161,4 +161,51 @@ if (age < 13) {
   console.log(firstName + " is a young man.");
 } else {
   console.log(firstName + " is a man.");
+}
+*/
+
+/**
+ * The Ternary Operator and Switch Statements
+ */
+
+var firstName = "John";
+var age = 22;
+
+// Ternary operator
+age >= 18
+  ? console.log(firstName + " drinks bear.")
+  : console.log(firstName + " drinks juice.");
+
+var drink = age >= 18 ? "bear" : "juice";
+console.log(drink);
+
+// Switch statement
+var job = "instructor";
+switch (job) {
+  case "teacher":
+  case "instructor":
+    console.log(firstName + " teaches kids how to code.");
+    break;
+  case "driver":
+    console.log(firstName + " drives an uber in Lisbon.");
+    break;
+  case "designer":
+    console.log(firstName + " designs beautiful websites.");
+    break;
+  default:
+    console.log(firstName + " does something else.");
+}
+
+switch (true) {
+  case age < 13:
+    console.log(firstName + " is a boy.");
+    break;
+  case age >= 13 && age < 20:
+    console.log(firstName + " is a teenager.");
+    break;
+  case age >= 20 && age < 30:
+    console.log(firstName + " is a young man.");
+    break;
+  default:
+    console.log(firstName + " is a man.");
 }
