5ca933c7615041fd81b2fe1262ea62e243f92347
Guillermo Olmedo <geomormx@gmail.com>
2020-02-29 01:59:50 +0800

    JS Basics: Loop, iteration

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -335,7 +335,7 @@ console.log(jane);
 /**
  * Objects and methods
  */
-
+/*
 var john = {
   firstName: "John",
   lastName: "Smith",
@@ -350,3 +350,41 @@ var john = {
 
 john.calcAge();
 console.log(john);
+*/
+
+/**
+ * Loops and iteration
+ */
+
+//  for loop
+for (var i = 1; i <= 20; i += 2) {
+  console.log(i);
+}
+
+var john = ["John", "Smith", 1990, "teacher", false, "blue"];
+for (var i = 0; i < john.length; i++) {
+  console.log(john[i]);
+}
+
+// while loop
+var i = 0;
+while (i < john.length) {
+  console.log(john[i]);
+  i++;
+}
+
+// continue and break statements
+for (var i = 0; i < john.length; i++) {
+  if (typeof john[i] !== "string") continue;
+  console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+  if (typeof john[i] !== "string") break;
+  console.log(john[i]);
+}
+
+// Looping backwards
+for (var i = john.length - 1; i >= 0; i--) {
+  console.log(john[i]);
+}
