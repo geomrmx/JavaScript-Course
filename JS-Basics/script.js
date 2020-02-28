72d53c6694127df211ced2b19ad93774916d95aa
Guillermo Olmedo <geomormx@gmail.com>
2020-02-28 10:52:46 +0800

    JS Basics: Array

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -243,7 +243,7 @@ yearsUntilRetirement(1969, "Jane");
 /**
  * Function Statements and Expressions
  */
-
+/*
 // Function expression
 var whatDoYouDo = function(job, firstName) {
   switch (job) {
@@ -261,3 +261,40 @@ var whatDoYouDo = function(job, firstName) {
 console.log(whatDoYouDo("teacher", "John"));
 console.log(whatDoYouDo("designer", "Jane"));
 console.log(whatDoYouDo("retired", "Mark"));
+*/
+
+/**
+ * Arrays
+ */
+
+// Intialize new array
+var names = ["John", "Mark", "Jane"];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[0]);
+console.log(names.length);
+
+// Mutate array data
+names[1] = "Ben";
+names[names.length] = "Mary";
+console.log(names);
+
+// Different data types
+var john = ["John", "Smith", 1990, "teacher", false];
+
+john.push("blue");
+john.unshift("Mr.");
+console.log(john);
+
+john.pop();
+john.pop();
+john.shift();
+console.log(john);
+
+console.log(john.indexOf(1990));
+
+var isDesigner =
+  john.indexOf("designer") === -1
+    ? "John is NOT a designer"
+    : "John IS a designer";
+console.log(isDesigner);
