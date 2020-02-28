1b84bf34bde1519df7d760fbb9f93db5d8378061
Guillermo Olmedo <geomormx@gmail.com>
2020-02-28 11:39:44 +0800

    JS Basics: Coding challenge 3

--- JS-Basics/challenge.js
+++ JS-Basics/challenge.js
@@ -39,7 +39,7 @@ John and Mike both play basketball in different teams. In the latest 3 games, Jo
 4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision.
 5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
  */
-
+/*
 var scoreJohn = (89 + 120 + 103) / 3;
 var scoreMike = (116 + 94 + 123) / 3;
 console.log(scoreJohn, scoreMike);
@@ -64,3 +64,44 @@ if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
 } else {
   console.log("There is a draw");
 }
+*/
+
+/**
+ * CODING CHALLENGE 3
+ */
+
+/*
+John and his family went on a holiday and went to a different restaurants. The billls were $124, $48 and $268.
+
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill).
+2) Containing all three final paid amounts (bill + tip).
+
+(NOTE: To calculate 20% of a value, simply multiply if with 20/100 = 0.02)
+*/
+
+function tipCalculator(bill) {
+  var percentage;
+
+  if (bill < 50) {
+    percentage = 0.2;
+  } else if (bill >= 50 && bill < 200) {
+    percentage = 0.15;
+  } else {
+    percentage = 0.1;
+  }
+
+  return percentage * bill;
+}
+
+var bills = [124, 48, 268];
+var tips = [
+  tipCalculator(bills[0]),
+  tipCalculator(bills[1]),
+  tipCalculator(bills[2])
+];
+var finalValues = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(tips);
+console.log(finalValues);
