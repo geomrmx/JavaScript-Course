acbf75c8a443658be93fe6c2cb5c623d76927bee
Guillermo Olmedo <geomormx@gmail.com>
2020-02-28 10:39:31 +0800

    JS Basics: Function expression

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -214,7 +214,7 @@ switch (true) {
 /**
  * Functions
  */
-
+/*
 function calculateAge(birthYear) {
   return 2020 - birthYear;
 }
@@ -238,3 +238,26 @@ function yearsUntilRetirement(year, firstName) {
 yearsUntilRetirement(1990, "John");
 yearsUntilRetirement(1948, "Mike");
 yearsUntilRetirement(1969, "Jane");
+*/
+
+/**
+ * Function Statements and Expressions
+ */
+
+// Function expression
+var whatDoYouDo = function(job, firstName) {
+  switch (job) {
+    case "teacher":
+      return firstName + " teaches kids how to code.";
+    case "driver":
+      return firstName + " drives a cab in Lisbon.";
+    case "designer":
+      return firstName + " designs beautiful websites.";
+    default:
+      return firstName + " does something else.";
+  }
+};
+
+console.log(whatDoYouDo("teacher", "John"));
+console.log(whatDoYouDo("designer", "Jane"));
+console.log(whatDoYouDo("retired", "Mark"));
