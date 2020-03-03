a34ec1bd3924145872141ea24886152108aa10ca
Guillermo Olmedo <geomormx@gmail.com>
2020-03-03 22:51:27 +0800

    Pig Game: Events, event listener, anonymous function

--- DOM-pig-game/app.js
+++ DOM-pig-game/app.js
@@ -13,11 +13,25 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector("#current-" + activePlayer).textContent = dice;
-
-var x = document.querySelector("#score-0").textContent;
-console.log(x);
-
 document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  // 1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display the result
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "images/dice-" + dice + ".png";
+
+  // 3. Update the round score IF the rolled number was NOT a 1
+});
+
+// document.querySelector("#current-" + activePlayer).textContent = dice;
+// var x = document.querySelector("#score-0").textContent;
+// console.log(x);
