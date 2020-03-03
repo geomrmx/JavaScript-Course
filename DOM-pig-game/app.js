cebc9236e39d3bbf02833022d62d095a6039b4bc
Guillermo Olmedo <geomormx@gmail.com>
2020-03-03 23:09:13 +0800

    Pig Game: HTML class - add/remove/toggle

--- DOM-pig-game/app.js
+++ DOM-pig-game/app.js
@@ -11,7 +11,7 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector(".dice").style.display = "none";
 
@@ -30,8 +30,27 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
   diceDOM.src = "images/dice-" + dice + ".png";
 
   // 3. Update the round score IF the rolled number was NOT a 1
+  if (dice !== 1) {
+    // Add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    // Next player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    // document.querySelector(".player-0-panel").classList.remove("active");
+    // document.querySelector(".player-1-panel").classList.add("active");
+
+    document.querySelector(".dice").style.display = "none";
+  }
 });
 
 // document.querySelector("#current-" + activePlayer).textContent = dice;
 // var x = document.querySelector("#score-0").textContent;
-// console.log(x);
