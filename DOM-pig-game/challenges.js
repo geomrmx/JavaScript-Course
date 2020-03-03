3859d56a76b5b978e79f03308233b104a0e4630c
Guillermo Olmedo <geomormx@gmail.com>
2020-03-04 03:25:00 +0800

    Pig Game: Coding challenge - part 3

--- DOM-pig-game/challenges.js
+++ DOM-pig-game/challenges.js
@@ -9,29 +9,25 @@ Change the game to follow these rules:
 
 var scores, roundScore, activePlayer;
 var gamePlaying;
-var lastDice;
 
 init();
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
   if (gamePlaying) {
     // 1. Random number
-    var dice = Math.floor(Math.random() * 6) + 1;
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
 
     // 2. Display the result
-    var diceDOM = document.querySelector(".dice");
-    diceDOM.style.display = "block";
-    diceDOM.src = "images/dice-" + dice + ".png";
+    document.getElementById("dice-1").style.display = "block";
+    document.getElementById("dice-2").style.display = "block";
+    document.getElementById("dice-1").src = "images/dice-" + dice1 + ".png";
+    document.getElementById("dice-2").src = "images/dice-" + dice2 + ".png";
 
     // 3. Update the round score IF the rolled number was NOT a 1
-    if (dice === 6 && lastDice === 6) {
-      // Player loses score
-      scores[activePlayer] = 0;
-      document.querySelector("#score-" + activePlayer).textContent = "0";
-      nextPlayer();
-    } else if (dice !== 1) {
+    if (dice1 !== 1 && dice2 !== 1) {
       // Add score
-      roundScore += dice;
+      roundScore += dice1 + dice2;
       document.querySelector(
         "#current-" + activePlayer
       ).textContent = roundScore;
@@ -39,8 +35,6 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
       // Next player
       nextPlayer();
     }
-
-    lastDice = dice;
   }
 });
 
@@ -67,7 +61,8 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
     // Check if player won the game
     if (scores[activePlayer] >= winningScore) {
       document.querySelector("#name-" + activePlayer).textContent = "Winner!";
-      document.querySelector(".dice").style.display = "none";
+      document.getElementById("dice-1").style.display = "none";
+      document.getElementById("dice-2").style.display = "none";
       document
         .querySelector(".player-" + activePlayer + "-panel")
         .classList.add("winner");
@@ -93,7 +88,8 @@ function nextPlayer() {
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
 
-  document.querySelector(".dice").style.display = "none";
+  document.getElementById("dice-1").style.display = "none";
+  document.getElementById("dice-2").style.display = "none";
 }
 
 document.querySelector(".btn-new").addEventListener("click", init);
@@ -104,7 +100,8 @@ function init() {
   roundScore = 0;
   gamePlaying = true;
 
-  document.querySelector(".dice").style.display = "none";
+  document.getElementById("dice-1").style.display = "none";
+  document.getElementById("dice-2").style.display = "none";
 
   document.getElementById("score-0").textContent = "0";
   document.getElementById("score-1").textContent = "0";
