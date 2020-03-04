b185e0b6eed06691e22bd0d560b189623140a4f2
Guillermo Olmedo <geomormx@gmail.com>
2020-03-05 02:26:06 +0800

    Advanced: Primitives vs objects

--- Advanced-JS/script.js
+++ Advanced-JS/script.js
@@ -36,7 +36,7 @@ console.log(mark.lastName);
 /**
  * Object create
  */
-
+/*
 var personProto = {
   calculateAge: function() {
     console.log(202 - this.yearOfBirth);
@@ -53,3 +53,38 @@ var jane = Object.create(personProto, {
   yearOfBirth: { value: 1969 },
   job: { value: "designer" }
 });
+*/
+
+/**
+ * Primitives vs objects
+ */
+
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a, b);
+
+// Objects
+var obj1 = {
+  name: "John",
+  age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age, obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+  name: "Jonas",
+  city: "Lisbon"
+};
+
+function change(a, b) {
+  a = 30;
+  b.city = "San Francisco";
+}
+
+change(age, obj);
+console.log(age, obj.city);
