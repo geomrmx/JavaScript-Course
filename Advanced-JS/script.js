3edbbc546d1c499d84b7a4b8d3e6865498b94fd7
Guillermo Olmedo <geomormx@gmail.com>
2020-03-05 03:40:25 +0800

    Advanced: Functions returning functions

--- Advanced-JS/script.js
+++ Advanced-JS/script.js
@@ -93,7 +93,7 @@ console.log(age, obj.city);
 /**
  * Passing functions as arguments
  */
-
+/*
 var years = [1990, 1965, 1937, 2005, 1998];
 
 function arrayCalc(arr, fn) {
@@ -127,3 +127,32 @@ var rates = arrayCalc(ages, maxHeartRate);
 console.log(ages);
 console.log(fullAges);
 console.log(rates);
+*/
+
+/**
+ * Functions returning functions
+ */
+
+function interviewQuestion(job) {
+  if (job === "designer") {
+    return function(name) {
+      console.log(name + ", can you please explain what UX design is?");
+    };
+  } else if (job === "teacher") {
+    return function(name) {
+      console.log("What subject do you teach, " + name + "?");
+    };
+  } else {
+    return function(name) {
+      console.log("Hello " + name + ", what do you do?");
+    };
+  }
+}
+
+var teacherQuestion = interviewQuestion("teacher");
+var designerQuestion = interviewQuestion("designer");
+
+teacherQuestion("John");
+designerQuestion("Jane");
+
+interviewQuestion("teacher")("Mark");
