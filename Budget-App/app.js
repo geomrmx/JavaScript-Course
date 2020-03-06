7327298c3603d93acfbd4b064d7e762706df372c
Guillermo Olmedo <geomormx@gmail.com>
2020-03-07 03:46:05 +0800

    Budgety: Clear input fields

--- Budget-App/app.js
+++ Budget-App/app.js
@@ -99,6 +99,22 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
 
+    clearFields: function() {
+      var fields, fieldsArr;
+
+      fields = document.querySelectorAll(
+        DOMStrings.inputDescription + ", " + DOMStrings.inputValue
+      );
+
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = "";
+      });
+
+      fieldsArr[0].focus();
+    },
+
     getDOMStrings: function() {
       return DOMStrings;
     }
@@ -130,6 +146,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     // 3. Add the item to the UI
     UICtrl.addListItem(newItem, input.type);
+
+    // 4. Clear the fields
+    UICtrl.clearFields();
 
     // 4. Calculate the budget
     // 5. Display the budget on the UI
