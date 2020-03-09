d6daf9f6cb37ce8fa3d1a7609a11f37fe86d2678
Guillermo Olmedo <geomormx@gmail.com>
2020-03-09 22:49:10 +0800

    ES6: initial project, `let` and `const`

---
+++ ES6/script.js
@@ -0,0 +1,49 @@
+/**
+ * `let` and `const`
+ */
+
+// ES5
+var name5 = "Jane Smith";
+var age5 = 23;
+name5 = "Jane Miller";
+console.log(name5);
+
+// ES6
+const name6 = "Jane Smith"; // Can't be updated later
+let age6 = 23;
+name6 = "Jane Miller"; // Making assignment error
+console.log(name6);
+
+// ES5
+function driversLicence(passedTest) {
+  if (passedTest) {
+    var firstName = "John";
+    var yearOfBirth = 1990;
+  }
+
+  console.log(
+    firstName +
+      ", born in " +
+      yearOfBirth +
+      ", is now officially allowed to drive a car."
+  );
+}
+
+driversLicence(true);
+
+// ES6
+function driversLicence(passedTest) {
+  if (passedTest) {
+    let firstName = "John";
+    const yearOfBirth = 1990;
+  }
+
+  console.log(
+    firstName +
+      ", born in " +
+      yearOfBirth +
+      ", is now officially allowed to drive a car."
+  ); // Making reference error
+}
+
+driversLicence(true);
