83600e7a983473a8db1b124a9db9d1d69adb34a2
Guillermo Olmedo <geomormx@gmail.com>
2020-03-10 23:00:22 +0800

    ES6: Class basic

--- ES6/script.js
+++ ES6/script.js
@@ -395,7 +395,7 @@ emily = new Person("Emily", 1983, "Diaz", "Spanish");
 /**
  * Maps
  */
-
+/*
 const question = new Map();
 question.set(
   "question",
@@ -431,3 +431,46 @@ for (let [key, value] of question.entries()) {
 
 const ans = parseInt(prompt("Write the correct answer"));
 console.log(question.get(ans === question.get("correct")));
+*/
+
+/**
+ * Classes
+ */
+
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getFullYear() - this.yearOfBirth;
+  console.log(age);
+};
+
+var john5 = new Person5("John", 1980, "teacher");
+john5.calculateAge();
+
+// ES6
+class Person6 {
+  constructor(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+
+  calculateAge() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+  }
+
+  static greeting() {
+    console.log("Hey there!");
+  }
+}
+
+const john6 = new Person6("John", 1990, "teacher");
+john6.calculateAge();
+
+Person6.greeting();
