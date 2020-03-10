44eaca1ffad2fd1fd6e0f3ce6fde0684aaf5798e
Guillermo Olmedo <geomormx@gmail.com>
2020-03-10 23:16:34 +0800

    ES6: Class inheritance

--- ES6/script.js
+++ ES6/script.js
@@ -452,6 +452,24 @@ Person5.prototype.calculateAge = function() {
 var john5 = new Person5("John", 1980, "teacher");
 john5.calculateAge();
 
+// ES5 Class inherit
+var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals) {
+  Person5.call(this, name, yearOfBirth, job);
+  this.olympicGames = olympicGames;
+  this.medals = medals;
+};
+
+Athlete5.prototype = Object.create(Person5.prototype);
+
+Athlete5.prototype.wonMedal = function() {
+  this.medals++;
+  console.log(this.medals);
+};
+
+var johnAthlete5 = new Athlete5("John", 1980, "swimmer", 3, 10);
+johnAthlete5.calculateAge();
+johnAthlete5.wonMedal();
+
 // ES6
 class Person6 {
   constructor(name, yearOfBirth, job) {
@@ -473,4 +491,23 @@ class Person6 {
 const john6 = new Person6("John", 1990, "teacher");
 john6.calculateAge();
 
-Person6.greeting();
+// Person6.greeting();
+
+// ES6 Class inherit
+class Athlete6 extends Person6 {
+  constructor(name, yearOfBirth, job, olympicGames, medals) {
+    super(name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+  }
+
+  wonMedal() {
+    this.medals++;
+    console.log(this.medals);
+  }
+}
+
+const johnAthlete6 = new Athlete6("John", 1990, "swimmer", 3, 10);
+
+johnAthlete6.wonMedal();
+johnAthlete6.calculateAge();
